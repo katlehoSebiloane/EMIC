91d994f8479016d759c26ab28445f175f9881147
katlehoSebiloane <63253802+katlehoSebiloane@users.noreply.github.com>
2021-04-26 11:43:37 +0200

    Delete EMIC/EMIC_UI/src/main/java/EMIC_UI directory

--- EMIC/EMIC_UI/src/main/java/EMIC_UI/file_trends.java
+++
@@ -1,9 +0,0 @@
-package EMIC_UI;
-
-import javafx.scene.layout.BorderPane;
-
-public class file_trends extends BorderPane {
-    file_trends(){
-        setStyle("-fx-background-color:#2D2D2D");
-    }
-}
