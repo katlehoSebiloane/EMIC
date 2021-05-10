71b36f6d7a1eaef2b947ffa6401a4eee581062a3
katlehoSebiloane <63253802+katlehoSebiloane@users.noreply.github.com>
2021-05-10 11:24:39 +0200

    Add files via upload

---
+++ EMIC_SERVER/src/main/java/EMIC_SERVER/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EMIC_SERVER;
+
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+
+/**
+ *
+ * @author KATLEHO SEBILOANE
+ */
+public class Main {
+    private static Socket socket = null;
+    private static ServerSocket server = null;
+
+    public static void main(String[] args) throws IOException {
+        server = new ServerSocket(15000);
+        System.out.println("The server is up and running....");
+        System.out.println(InetAddress.getLocalHost());
+
+        while(true){
+            socket = server.accept();
+            Handler hlr = new Handler(socket);
+            Thread t = new Thread(hlr);
+            t.start();
+        }
+
+    }
+
+}
